60c0358eb047df2951842aea997e9970953a1037
Bernard-VERA <bvmars@hotmail.com>
2026-03-28 14:47:26 +0100

    Nettoyage des fichiers inutiles

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,15 @@
-import { useState } from 'react'
-import faviconLogo from './assets/favicon.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <section id="center">
+    
       <div>
-        <img src={faviconLogo} className="car" alt="car logo" />
         <h1>AutoDépenses</h1>
         <p>
           Comment gérer les dépenses de vos véhicules
         </p>
       </div>
-      <button
-        className="counter"
-        onClick={() => setCount((count) => count + 1)}
-      >
-        Count is {count}
-      </button>
-    </section>
+    
   )
 }
 
