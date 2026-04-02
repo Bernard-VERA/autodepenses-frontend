2c43aa2c05b9192ac4dcffb330c95fab463a3239
Bernard-VERA <bvmars@hotmail.com>
2026-04-02 17:59:42 +0200

    correction de vite.config.js pour bug github pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/autodepenses-frontend/', // ← très important pour GitHub Pages
   plugins: [react()],
 })
