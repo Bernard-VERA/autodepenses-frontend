fcae3320645e2b71663693ae32ab2c1b0a3f6c2f
Bernard-VERA <bvmars@hotmail.com>
2026-04-02 20:12:55 +0200

    Ajout de commentaires pour le futur déploiement sur VERCEL

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/autodepenses-frontend/',
+  base: '/autodepenses-frontend/', // Cette ligne sera a supprimer pour déployer sur Vercel (Ajoutée uniquement pour le déploiement sur Github Pages)
   plugins: [react()],
 })
