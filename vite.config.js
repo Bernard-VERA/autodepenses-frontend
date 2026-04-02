3b2731edfd0a4341213caba7ab91cc1f28038a43
Bernard-VERA <bvmars@hotmail.com>
2026-04-02 18:40:35 +0200

    orrection du nom du fichier Vehicles.jsx + vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/autodepenses-frontend/', // ← très important pour GitHub Pages
   plugins: [react()],
 })
